71ff971b97d8725e0778a5c88da89f733cefef7c
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-02 04:34:12 +0530

    Effect for catch user action

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,26 @@
 //External Imports
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 //Internal Imports
+
 import SingleVideoDetail from "../singleVideoDetail/SingleVideoDetail";
 import Category from "../../components/category/Category";
+import { onAuthStateChanged } from "firebase/auth";
+import { authdb } from "../../store/redux-operation/auth/auth-action";
+import { ActionCreator } from "../../utils/action-creator";
 
 const Home = () => {
+  const { isLogIn } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
+  console.log(isLogIn);
+  useEffect(() => {
+    onAuthStateChanged(authdb, (user) => {
+      if (user) {
+        dispatch(ActionCreator("USER_LOGIN", user));
+      }
+    });
+  }, []);
   return (
     <div>
       <Category />

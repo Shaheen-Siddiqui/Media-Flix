3a608a59504f16568c1b0e5d29a8b239451b6498
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-04 14:52:08 +0530

    useEffect onAuthStateChange for catching

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,22 +5,9 @@ import { useSelector, useDispatch } from "react-redux";
 
 import SingleVideoDetail from "../singleVideoDetail/SingleVideoDetail";
 import Category from "../../components/category/Category";
-import { onAuthStateChanged } from "firebase/auth";
-import { authdb } from "../../store/redux-operation/auth/auth-action";
-import { ActionCreator } from "../../utils/action-creator";
 
 const Home = () => {
-  const { isLogIn } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
 
-  console.log(isLogIn);
-  useEffect(() => {
-    onAuthStateChanged(authdb, (user) => {
-      if (user) {
-        dispatch(ActionCreator("USER_LOGIN", user));
-      }
-    });
-  }, []);
   return (
     <div>
       <Category />

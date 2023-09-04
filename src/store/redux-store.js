abfff8f0f123934813c6e0f22f299bb6b82ccc9d
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-04 14:52:27 +0530

    composeWithDevTools amd middleware

--- src/store/redux-store.js
+++ src/store/redux-store.js
@@ -1,4 +1,9 @@
-import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
 import { RootReducer } from "./root-reducer";
+import thunk from "redux-thunk";
 
-export const store = createStore(RootReducer);
+export const store = createStore(
+  RootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);

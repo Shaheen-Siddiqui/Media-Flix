117c6cafaf7d596b795788953b071c9e3377b4ef
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-04 14:41:33 +0530

    chor: logout button state change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,40 @@
 import "./Header.css";
+import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faBars } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { authdb } from "../../store/redux-operation/auth/auth-action";
+import { ActionCreator } from "../../utils/action-creator";
+import { authConstants } from "../../store/redux-operation/auth/auth-constants";
 
- const Header = ({ setToggleSidebar }) => {
+const Header = ({ setToggleSidebar }) => {
+
+  const { SIGN_OUT, USER_LOGIN } = authConstants;
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { isLogIn } = useSelector((state) => state.auth);
+
+  const signOutUser = () => {
+    navigate("/login");
+    signOut(authdb);
+    dispatch(ActionCreator(SIGN_OUT));
+  };
+
+
+  useEffect(() => {
+    onAuthStateChanged(authdb, (user) => {
+      if (user) {
+        dispatch(ActionCreator(USER_LOGIN, user));
+      } else {
+        dispatch(ActionCreator(SIGN_OUT));
+      }
+    });
+  }, []);
+  
+
+
   return (
     <header className="header-case">
       <div className="logo-bar-case">
@@ -37,10 +67,14 @@ import { useNavigate } from "react-router-dom";
       </div>
       <div className="right-nav">
         <h3>Explore</h3>
-        <button onClick={() => navigate("/login")}>Logout</button>
+        {!isLogIn ? (
+          <button onClick={() => navigate("/login")}>Login</button>
+        ) : (
+          <button onClick={signOutUser}>Logout</button>
+        )}
       </div>
     </header>
   );
 };
 
-export  {Header as default}
+export { Header as default };

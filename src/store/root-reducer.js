04284c500ce4bf78a38e3d8f34e8d21ea942ab8a
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-05 16:54:39 +0530

    feat: new reducer (videoReducer)

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { AuthReducer } from "./redux-operation/auth/auth-reducer";
+import { VideoReducer } from "./redux-operation/video/video-reducer";
 
 export const RootReducer = combineReducers({
   auth: AuthReducer,
+  video:VideoReducer
 });

f0388dc9149ae8fe0bb1784d5905b35d903f706a
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-07 10:42:27 +0530

    useParams()

--- src/pages/singleVideoDetail/SingleVideoDetail.jsx
+++ src/pages/singleVideoDetail/SingleVideoDetail.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { AiOutlineLike, AiFillLike } from "react-icons/ai";
 import { BsStopwatch } from "react-icons/bs";
 import { PiPlaylistBold, PiNotepadBold } from "react-icons/pi";
@@ -6,48 +6,64 @@ import { PiPlaylistBold, PiNotepadBold } from "react-icons/pi";
 import "./SingleVideoDetail.css";
 import NoteModal from "../../components/modalBox/NoteModal";
 import PlaylistModal from "../../components/modalBox/PlaylistModal";
+import { useParams } from "react-router";
+import { useSelector } from "react-redux";
 
 const SingleVideoDetail = () => {
+  const { videoID } = useParams();
   const [showNoteModal, setShowNoteModal] = useState(false);
-  const [showPlayListModal, setShowPlayListModal]=useState(false)
+  const [showPlayListModal, setShowPlayListModal] = useState(false);
+  const { ycVideoCase } = useSelector((state) => state.video);
 
+  const selectedVideo = ycVideoCase.find((item) => item?._id == videoID);
+
+  // const {
+  //   _id,
+  //   title,
+  //   views,
+  //   thumbnail,
+  //   src,
+  //   category,
+  //   creator,
+  //   videoThumbnail,
+  // } = selectedVideo;
   return (
     <>
-    {
-      showPlayListModal &&
-    <PlaylistModal setShowPlayListModal={setShowPlayListModal}/>
-    }
+      {showPlayListModal && (
+        <PlaylistModal setShowPlayListModal={setShowPlayListModal} />
+      )}
       {showNoteModal && <NoteModal setShowNoteModal={setShowNoteModal} />}
 
       <div className="detail-case">
-        <iframe
-          height="460"
-          title="Embedded content from Example Website"
-          src="https://www.youtube.com/embed/eos4g-kRtXo?start=22"
-          frameborder="0"
-          allow="autoplay; encrypted-media"
-          allowFullScreen
-        ></iframe>
-        <h2>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,{" "}
-        </h2>
+        <center>
+          <img
+            height="400"
+            title={selectedVideo?.title}
+            src={selectedVideo?.videoThumbnail}
+          />
+        </center>
+
+        <h2>{selectedVideo?.title}</h2>
         <div className="icon-view-case">
-          <h3>120k views.</h3>
+          <h3>{selectedVideo?.views} views.</h3>
           <div>
             <span>
               <AiOutlineLike size={20} />
-              {/* <AiFillLike/> */}
+              {/* <AiFillLike size={20}/> */}
             </span>
 
             <span>
-              <PiPlaylistBold size={20} onClick={()=>setShowPlayListModal(true)}/>
+              <PiPlaylistBold
+                size={20}
+                onClick={() => setShowPlayListModal(true)}
+              />
             </span>
             <span>
               <BsStopwatch size={20} />
               {/* <BsStopwatchFill size={40}/> */}
             </span>
-            <span onClick={()=>setShowNoteModal(true)}>
-              <PiNotepadBold size={20}  />
+            <span onClick={() => setShowNoteModal(true)}>
+              <PiNotepadBold size={20} />
             </span>
           </div>
         </div>
@@ -55,10 +71,7 @@ const SingleVideoDetail = () => {
         <div className="comment-space">
           <h3 className="comment-space-text">Comment Section</h3>
           <div>
-            <img
-              src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
-              alt=""
-            />
+            <img src={selectedVideo?.thumbnail} alt="" />
             <p>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
               ipsum!

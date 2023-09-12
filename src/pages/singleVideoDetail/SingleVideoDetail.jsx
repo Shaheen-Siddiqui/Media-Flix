0e623fd757332e50769fcb8ebb3e6ad46c1153c4
Shaheen Siddiqui <95218912+Shaheen-Siddiqui@users.noreply.github.com>
2023-09-12 16:13:05 +0530

    Update SingleVideoDetail.jsx

--- src/pages/singleVideoDetail/SingleVideoDetail.jsx
+++ src/pages/singleVideoDetail/SingleVideoDetail.jsx
@@ -10,7 +10,7 @@ import { GrEdit } from "react-icons/gr";
 
 //Internal Imports
 import "./SingleVideoDetail.css";
-import {NoteModal} from "../../components/modalBox/NoteModal";
+//import {NoteModal} from "../../components/modalBox/NoteModal";
 import PlaylistModal from "../../components/modalBox/PlaylistModal";
 import { isWatchLaterImage } from "../../store/redux-operation/video/video-action";
 import { ActionCreator } from "../../utils/action-creator";
@@ -58,7 +58,7 @@ const SingleVideoDetail = () => {
           selectedVideo={selectedVideo}
         />
       )}
-      {showNoteModal && (
+{/*       {showNoteModal && (
         <NoteModal
           setIsEdit={setIsEdit}
           isEdit={isEdit}
@@ -66,7 +66,7 @@ const SingleVideoDetail = () => {
           setShowNoteModal={setShowNoteModal}
           selectedVideo={selectedVideo}
         />
-      )}
+      )} */}
 
       <div className="detail-case">
         <center>

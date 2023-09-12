287d9b2ea3f0a86d8a12e8481776ade8373d4b28
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-12 12:21:28 +0530

    feat: playlist reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { AuthReducer } from "./redux-operation/auth/auth-reducer";
 import { VideoReducer } from "./redux-operation/video/video-reducer";
+import { PlaylistReducer } from "./redux-operation/playlist/playlist-reducer";
 
 export const RootReducer = combineReducers({
   auth: AuthReducer,
-  video:VideoReducer
+  video:VideoReducer,
+  playlist:PlaylistReducer
 });

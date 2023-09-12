8bca55fc66ce45e2decaf61212bf651d34e1ebb3
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-12 12:15:06 +0530

    feat: Modal for make playlist

--- src/components/ModalBox/PlaylistModal.jsx
+++ src/components/ModalBox/PlaylistModal.jsx
@@ -1,39 +1,98 @@
 import React from "react";
-import {ImCross} from 'react-icons/im'
+import { ImCross } from "react-icons/im";
+import { useDispatch, useSelector } from "react-redux";
 
+//External Imports:-
 import "../../pages/authentication/Authentication.css";
 import "./ModalBox.css";
+("KG ");
+import { ActionCreator } from "../../utils/action-creator";
+import { playlistConstant } from "../../store/redux-operation/playlist/playlist-constant";
 
-const PlaylistModal = ({setShowPlayListModal}) => {
+const PlaylistModal = ({ setShowPlayListModal, selectedVideo }) => {
+  const {
+    ADD_CONTAINER,
+    CREATE_NEW_PLAYLIST,
+    TOGGLE_CHECKBOX,
+    DELETE_PLAYLIST,
+  } = playlistConstant;
+  const { listContainer, playlistCase } = useSelector(
+    (state) => state.playlist
+  );
 
-const submitHandler=(event)=>{
-  event.preventDefault()
-}
+  const dispatch = useDispatch();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    dispatch(ActionCreator(CREATE_NEW_PLAYLIST));
+  };
 
   return (
-    <div>
-      <form className="modal-box" onSubmit={submitHandler}>
-        <div className="modal-content">
-          <div className="checkbox-wrapper-1 " id="face-tofas">
-            <div>
-              
-            <input className="substituted" type="checkbox" id="lowstock" />
-            <label htmlFor="lowstock">Daily Morning Habit</label>
-            </div>
+    <form className="modal-box" onSubmit={submitHandler}>
+      <div className="modal-content">
+        <div className="checkbox-wrapper-1 " id="face-tofas">
+          <div>
+            {playlistCase.map((item, index) => {
+              const { listName, listCase, _id } = item;
+              return (
+                <div key={index} className="check-padding">
+                  <input
+                    checked={listCase.some((findItem)=>findItem._id==selectedVideo._id)}
+                    onChange={(event) =>
+                      event.target.checked === true
+                        ? dispatch(
+                            ActionCreator(TOGGLE_CHECKBOX, {
+                              toggleId: _id,
+                              ItemToAdd: selectedVideo,
+                            })
+                          )
+                        : dispatch(
+                            ActionCreator(DELETE_PLAYLIST, {
+                              listedItemId: selectedVideo._id,
+                              _id,
+                            })
+                          )
+                    }
+                    
+                    className="substituted"
+                    name="checkplaylist"
+                    type="checkbox"
+                    id={_id}
+                  />
+                  <label htmlFor={_id}>{listName}</label>
+                </div>
+              );
+            })}
+          </div>
 
-            <span className="icon-cursor" onClick={()=>setShowPlayListModal(false)}>
-              <ImCross/>
-            </span>
-          </div>
-          <div className="password-case playlist-content">
-            <input placeholder="Confirm Password" />
-          </div>
-          <button className="input-box submit-btn" id="playlist-btn">
-            Create New Playlist
-          </button>
+          <span
+            className="icon-cursor"
+            onClick={() => setShowPlayListModal(false)}
+          >
+            <ImCross />
+          </span>
         </div>
-      </form>
-    </div>
+        <div className="password-case playlist-content">
+          <input
+            required
+            autoFocus
+            placeholder="Playlist Name"
+            value={listContainer.listName}
+            onChange={(event) =>
+              dispatch(ActionCreator(ADD_CONTAINER, event.target.value))
+            }
+          />
+        </div>
+        <button
+          className="input-box submit-btn"
+          id="playlist-btn"
+          type="submit"
+        >
+          Create New Playlist
+        </button>
+      </div>
+    </form>
   );
 };
 
